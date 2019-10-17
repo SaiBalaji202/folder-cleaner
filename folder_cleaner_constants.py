a50eef2f5c8f3c7f45be60a08b2be4962d501672
saibalaji202@gmail.com <saibalaji202@gmail.com>
2019-10-17 10:05:08 +0530

    Added all the possible messages as a constant

---
+++ folder_cleaner_constants.py
@@ -0,0 +1,10 @@
+"""
+A Module that contains some constant messages for displaying log message when displaying spinner
+"""
+
+MSG_DIR_SCAN = 'Scaning Directory'
+MSG_DIR_CREATE = 'Creating Directory'
+MSG_DIR_DELETE = 'Deleting Directory'
+MSG_FILE_MOVE = 'Moving Files'
+MSG_SCAN = 'Scaning Directory & Moving Files'
+MSG_SET_ENV = 'Setting up Environment'
